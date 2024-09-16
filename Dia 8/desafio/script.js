ae7e49e221b5f9b005853a6fb3194c295f5366e2
joaodelabio <joaogabriel.delabio@gmail.com>
2024-09-15 21:12:35 -0300

    consegui obrigado a todos que acreditaram em mim

--- Dia 8/desafio/script.js
+++ Dia 8/desafio/script.js
@@ -1,1 +1,24 @@
-desisto
+let corAtual = 0;
+const interval = 2000;
+
+function mudarCor() {
+    const vermelho = document.getElementById('vermelho');
+    const amarelo = document.getElementById('amarelo');
+    const verde = document.getElementById('verde');
+
+    vermelho.classList.remove('ativo');
+    amarelo.classList.remove('ativo');
+    verde.classList.remove('ativo');
+
+    if (corAtual === 0) {
+        vermelho.classList.add('ativo');
+    } else if (corAtual === 1) {
+        amarelo.classList.add('ativo');
+    } else if (corAtual === 2) {
+        verde.classList.add('ativo');
+    }
+
+    corAtual = (corAtual + 1) % 3; //ciclo
+}
+
+setInterval(mudarCor, interval);
