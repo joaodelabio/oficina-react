2262fc56ca0382c3d48a8b85d6d2105c8f2b5931
joaodelabio <joaogabriel.delabio@gmail.com>
2024-09-29 16:02:49 -0300

    dor de cabeca

--- Revisao/Extras/filtrar-primos/script.js
+++ Revisao/Extras/filtrar-primos/script.js
@@ -1,1 +1,27 @@
-/* filtrar primos entre 2 elementos de um array. -> [10, 40], retornar primos de 10 a 40. */
+/* filtrar primos entre 2 elementos de um array. -> [10, 40], retornar primos que estao entre 10 e 40. */
+
+function numPrimo(numero) {
+    if (numero <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i * i <= numero; i++) {
+        if (numero % i === 0) {
+            return false;
+        }    
+    }
+    return true;
+}
+
+function listaElementos(num1, num2) {
+    let primos = [];
+    for (let numero = num1; numero <= num2; numero++) {
+        if (numPrimo(numero)) {
+            primos.push(numero)
+        }
+    }
+    return primos;
+}
+
+let primos = listaElementos(10, 40);
+console.log(primos);
