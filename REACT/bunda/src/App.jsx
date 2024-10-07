11ee0c12326bef9981407b21f4fbc7ca3d45b859
João Gabriel Assunção de Labio <joao.labio@dinfor.uniceub>
2024-10-07 18:57:56 -0300

    riekiti

---
+++ REACT/bunda/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+function App() {
+  //JAVASCRIPT
+  return (
+  <>
+    <Header/>
+    <h1>2050 homens comeram a minha bunda</h1>
+  </>
+  )
+}
+
+export default App
---
+++ main.jsx
@@ -0,0 +1,24 @@
+o ceub tem convenio com a amazon 2050 homens gozan
+acho q a gente pode ganhar um pc parceiro da amazon daium
+treinamento de docente da amazon, aparentemente
+e vai ter lab fisico da amazon
+como faz isSecureContext
+
+acho q vai sair AudioDestinationNode, n sei se esse semestre mas ainda vai ser aprovado
+pq tem mt lab fudido
+de gratis?
+
+setInterval
+0 reias? SVGImageElement, 
+qualquer um ou tem vaga limitada
+ainda não sei, ainda não foi liberado o PromiseRejectionEvent, mas provavelmente vai ser limitado mas tendo varias vezes duraante os ssemestres
+eu sou mulher
+
+tlgd q se vc ficar mt bom nesse curso da amzon eles te chamam pra estagiar
+
+e como faz esse curso?
+
+AnimationTimeline
+
+vcs tbm deveriam ver o ceub mobilidade, vai q vcs entramai tem a oportunidade de estudar fora do pais e vc escolhe o ByteLengthQueuingStrategy
+odne o ceub movilidade?
